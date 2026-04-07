523caa7d347ba91e9b40a8a6edba91f8ce2ef45f
kat-robi <robineyestudios@gmail.com>
2026-04-07 11:13:28 -0500

    Increase scale range in randomize function

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ let divs = document.querySelectorAll(".grid-container div");
 //Define Randomize function
 function randomize() {
   divs.forEach(function (div) {
-    let scale = randomNumber(0.5, 1.2);
+    let scale = randomNumber(0.5, 2);
     let translateX = randomNumber(0, 50);
     let translateY = randomNumber(0, 20);
     let rotate = randomNumber(0, 360);
